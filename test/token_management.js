2acd6b6534d563a0925fbbf3c0a61168c6b72f2b
Andy.Chen <f107110126@gmail.com>
2019-12-11 22:55:53 +0800

    test with error, ignore

---
+++ test/token_management.js
@@ -0,0 +1,9 @@
+// var token_management = artifacts.require("token_management");
+
+// contract("token_management", function(_accounts) {
+//   it("should assert true", function(done) {
+//     token_management.deployed();
+//     assert.isTrue(true);
+//     done();
+//   });
+// });
---
+++ test/token_transfer.js
@@ -0,0 +1,10 @@
+// const TokenZendR = artifacts.require('TokenZendR');
+// var token_transfer = artifacts.require("token_transfer");
+
+// contract("token_transfer", function(_accounts) {
+//   it("should assert true", function(done) {
+//     token_transfer.deployed();
+//     assert.isTrue(true);
+//     done();
+//   });
+// });
